69eda24492707ad52b1f9f6af486b90e9c782621
Sydney Tolbert <sydneykaturah@lawn-143-215-87-25.lawn.gatech.edu>
2018-11-08 21:30:25 -0500

    added folder for screenshots

--- amazonSchema.sql
+++ amazonSchema.sql
@@ -2,3 +2,17 @@ DROP DATABASE IF EXISTS amazonHW_db;
 CREATE DATABASE amazonHW_db;
 USE amazonHW_db;
 
+CREATE TABLE forSale (
+	id INT NOT NULL AUTO_INCREMENT,
+	item_name VARCHAR(100) NOT NULL,
+	department VARCHAR(45) NOT NULL,
+	price INT default 0,
+	inventory INT default 0,
+	PRIMARY KEY (id)
+);
+
+INSERT INTO forSale (item_name, department, price, inventory)
+VALUES ("tshirt", "clothing", 10, 5),
+	("skirt", "clothing", 15, 5),
+    ("tennis shoes", "clothing", 25, 6),
+    ("socks", "clothing", 5, 10);
